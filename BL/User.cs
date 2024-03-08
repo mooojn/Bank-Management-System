6e842ad300602bb1bfc734c61b479c73d911d44e
mooojn <moontariq7867@gmail.com>
2024-03-08 12:52:19 +0500

    user class

---
+++ BL/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MainBusinessApp
+{
+    internal class User
+    {
+        // objs
+        private string name;
+        private string password;
+        private int cash;
+        // constructor for initializing
+        public User(string userName, string userPass)
+        {
+            name = userName;
+            password = userPass;
+            cash = 0;  // default val
+        }
+        public bool AddCash(int depositAmount)
+        {
+            // error encountered so returning
+            if (depositAmount < 0)
+                return false;
+            // adding cash
+            cash += depositAmount;
+            return true;
+        }
+        public bool WithdrawCash(int withdrawAmount)
+        {
+            // error encountered so returning
+            if (withdrawAmount < 0 || withdrawAmount > cash)
+                return false;
+            // withdrawing cash
+            cash -= withdrawAmount;
+            return true;
+        }
+        public void ShowCash()
+        {
+            Console.Write($"Your total cash holdings holding: ${cash}");
+        }
+    }
+}
