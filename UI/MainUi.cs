3e60b34a6d1966b91bf0f96988577c5dfdb19eb7
mooojn <moontariq7867@gmail.com>
2024-03-08 15:33:54 +0500

    Improved Ui

--- UI/MainUi.cs
+++ UI/MainUi.cs
@@ -10,16 +10,22 @@ namespace MainBusinessApp
     {
         public static string Menu()
         {
-            Console.WriteLine("1. Sign In As Admin...");
-            Console.WriteLine("2. Sign In As User...");
-            Console.WriteLine("3. Sign Up...");
-            Console.WriteLine("4. Exit......");
+            Console.Clear();
+            // welcome message
+            Console.WriteLine("......Welcome to the Bank......\n");
+            // menu
+            Console.WriteLine("1. Sign-in as an Administrator");
+            Console.WriteLine("2. Sign-in as a User");
+            Console.WriteLine("3. Sign-up as a User");
 
-            Console.Write("Enter your choice: ");
-            return Console.ReadLine();
+            Console.WriteLine("4. Exit");
+
+            // input
+            return UtilUi.GetChoice();
         }
-        public static string user_menu()
+        public static string UserMenu()
         {
+            Console.Clear();
             // menu
             Console.WriteLine("1. Check Portfolio");
             Console.WriteLine("2. Deposit Cash");
@@ -30,21 +36,21 @@ namespace MainBusinessApp
             Console.WriteLine("6. Logout");
 
             // getting input
-            Console.Write("Enter your choice: ");
-            return Console.ReadLine();
+            return UtilUi.GetChoice();
         }
-        public static string admin_menu()
+        public static string AdminMenu()
         {
+            Console.Clear();
             // menu
             Console.WriteLine("1. Add New User");
             Console.WriteLine("2. View Users");
             Console.WriteLine("3. Change User Name");
             Console.WriteLine("4. Delete User");
+
             Console.WriteLine("5. Logout");
 
             // getting input
-            Console.Write("Enter your choice: ");
-            return Console.ReadLine();
+            return UtilUi.GetChoice();
         }
     }
 }
