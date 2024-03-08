7bfd0c11ca928d69d73b32eeec922c197558ced9
mooojn <moontariq7867@gmail.com>
2024-03-08 15:15:46 +0500

    user trans history classs

---
+++ BL/History.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainBusinessApp
+{
+    internal class History
+    {
+        private string type;
+        private int amount;
+        private DateTime date;
+        public History(string type, int amount, DateTime date)
+        {
+            this.type = type;
+            this.amount = amount;
+            this.date = date;
+        }
+    }
+}
