1713b7a91545c3395c45fcd88bf5ccb6c6aa0007
mooojn <moontariq7867@gmail.com>
2024-03-16 17:34:12 +0500

    Add Deposit, Withdraw, And View Cash func

---
+++ DL/UserFunc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainBusinessApp
+{
+    internal class UserFunc
+    {
+        public static void CheckPortfolio()
+        {
+            Program.connection.Open();
+            
+            string query = "Select Cash from Users where userId = @userId";
+
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@userId", Program.currentUserId);
+            SqlDataReader reader = cmd.ExecuteReader(); 
+            
+            if (reader.Read()) 
+                Console.WriteLine("Cash: " + reader["Cash"]);
+            Console.ReadKey();
+            
+            Program.connection.Close();
+        }
+        public static void DepositCash()
+        {
+            Program.connection.Open();
+            
+            Console.Write("Enter the amount to deposit: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            
+            string query = "Update Users set Cash = Cash + @amount where userId = @userId";
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@amount", amount);
+            cmd.Parameters.AddWithValue("@userId", Program.currentUserId);
+            
+            cmd.ExecuteNonQuery();
+            
+            Program.connection.Close();
+            UtilUi.Success("Cash has been deposited");
+        }
+        public static void WithdrawCash()
+        {
+            Program.connection.Open();
+
+            Console.Write("Enter the amount to withdraw: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            string query = "Update Users set Cash = Cash - @amount where userId = @userId";
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@amount", amount);
+            cmd.Parameters.AddWithValue("@userId", Program.currentUserId);
+
+            cmd.ExecuteNonQuery();
+
+            Program.connection.Close();
+            UtilUi.Success("Cash has been deposited");
+        }
+    }
+}
